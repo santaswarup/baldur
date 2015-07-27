fe1b0d9f88d56bb51447dcf16380bd57f7515267
Eric Meisel <eric.meisel@influencehealth.com>
2015-07-27 20:30:59 +0000

    removing unused checkpoint dir

--- rundocker.sh
+++ rundocker.sh
@@ -4,7 +4,6 @@ CONTAINER_NAME=${1:-baldur}
 docker run --rm --net host \
   --name $CONTAINER_NAME \
   -v /usr/share/spark/conf:/usr/local/spark/conf \
-  -v /usr/share/checkpoint/person_identity:/checkpoint \
   -v /secure:/secure \
   -v /chi-ms-files-02:/chi-ms-files-02 \
   -v /mnt/talend/dev:/data \
