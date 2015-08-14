94d435410585e6c126459af0401a6da35a943dd5
Eric Meisel <eric.meisel@influencehealth.com>
2015-08-14 21:15:46 +0000

    changing run scripts

---
+++ run_physician_office.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+$SPARK_HOME/bin/spark-submit --master spark://az-preds-ela-01.ecosmart.local:7077 \
+  --total-executor-cores 4 \
+  --jars target/scala-2.11/baldur-assembly-1.0.jar \
+  target/scala-2.11/baldur_2.11-1.0.jar \
+  -i $1 \
+  -o /data/Baldur/input \
+  -c piedmont \
+  --source-type "physician office" \
+  --source epic \
+  --metadata.broker.list "az-kafka-01.ecosmart.local:9092,az-kafka-02.ecosmart.local:9092"
--- rundocker.sh
+++ rundocker.sh
@@ -5,7 +5,7 @@ docker run --rm --net host \
   --name $CONTAINER_NAME \
   -v /usr/share/spark/conf:/usr/local/spark/conf \
   -v /secure:/secure \
-  -v /chi-ms-files-02:/chi-ms-files-02 \
-  -v /mnt/talend/dev:/data \
+  -v /media/chi-ms-files-02:/chi-ms-files-02 \
+  -v /media/talend/dev:/data \
   -v $PWD:/app \
   -it etspaceman/spark-docker 
