1251240fa29e2f65df9f92271d844f04a6a229f3
Eric Meisel <eric.meisel@influencehealth.com>
2015-08-28 17:54:52 +0000

    tweaking import_identity.sh

--- import_identity.sh
+++ import_identity.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 $SPARK_HOME/bin/spark-submit \
-  --master local[*] \
-  --total-executor-cores 4 \
+  --master spark://az-preds-ela-01.ecosmart.local:7077 \
+  --total-executor-cores 32 \
   --jars target/scala-2.11/baldur-assembly-1.0.jar \
   --class com.influencehealth.baldur.identity_load.IdentityLoadApp \
-  --conf spark.cassandra.connection.host=localhost \
+  --conf spark.app.kafka.metadata.broker.list="az-kafka-01.ecosmart.local:9092,az-kafka-02.ecosmart.local:9092" \
+  --conf spark.cassandra.connection.host=az-datahub-01.ecosmart.local \
   target/scala-2.11/baldur_2.11-1.0.jar \
   -i $1 \
   --inputSource "baldur" \
-  --metadata.broker.list "localhost:9092"
+  --metadata.broker.list "az-kafka-01.ecosmart.local:9092,az-kafka-02.ecosmart.local:9092"
