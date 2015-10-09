21e06d89fc28e689ab5cb19e84a8b8de3106956c
Eric Meisel <eric.meisel@influencehealth.com>
2015-10-09 17:24:45 -0500

    cassandra connection properties

--- delete_code_changes.sh
+++ delete_code_changes.sh
@@ -5,4 +5,6 @@ $SPARK_HOME/bin/spark-submit \
   --total-executor-cores 4 \
   --jars target/scala-2.11/baldur-assembly-1.0.jar \
   --class com.influencehealth.baldur.DeleteCodeChanges \
+  --conf spark.app.kafka.metadata.broker.list="az-kafka-01.ecosmart.local:9092" \
+  --conf spark.cassandra.connection.host=az-datahub-01.ecosmart.local \
   target/scala-2.11/baldur_2.11-1.0.jar
